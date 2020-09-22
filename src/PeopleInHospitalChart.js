d6da80ae4ffbcf080be6b63aea57797acfcfdf2f
Antoine Guenet <age@odoo.com>
2020-09-22 14:26:47 +0200

    [REF] PeopleInHospitalChart: make into class

--- src/PeopleInHospitalChart.js
+++ src/PeopleInHospitalChart.js
@@ -1,60 +1,63 @@
 import React from 'react';
+import memoize from 'memoize-one';
 import { Chart } from 'react-charts';
 import { ResizableArea } from 'react-resizable-area';
 
-function PeopleInHospitalChart(props) {
-    let hospiData = props.data?.hospi || [];
-    if (props.start) {
-        hospiData = hospiData.filter(item => new Date(item.DATE) > props.start);
-    }
-    const dates = new Set(hospiData?.map(item => item.DATE));
-    const points = [];
-    for (const date of dates) {
-        const items = hospiData.filter(item => item.DATE === date);
-        const patients = items.reduce((a, b) => a + b.TOTAL_IN, 0) || 0;
-        points.push({x: new Date(date), y: patients});
-    }
-    const data = React.useMemo(
-        () => [
-        {
-            label: 'Number of people in the hospital',
-            data: points,
-        },
-        ], [points]
-    );
-    const series = React.useMemo(
-        () => ({
-            showPoints: false,
-        }),
-        []
-    );
+class PeopleInHospitalChart extends React.Component {
+    state = {};
+    render() {
+        let hospiData = this.props.data?.hospi || [];
+        if (this.props.start) {
+            hospiData = hospiData.filter(item => new Date(item.DATE) > this.props.start);
+        }
+        const dates = new Set(hospiData?.map(item => item.DATE));
+        const points = [];
+        for (const date of dates) {
+            const items = hospiData.filter(item => item.DATE === date);
+            const patients = items.reduce((a, b) => a + b.TOTAL_IN, 0) || 0;
+            points.push({x: new Date(date), y: patients});
+        }
+        const data = memoize(
+            () => [
+            {
+                label: 'Number of people in the hospital',
+                data: points,
+            },
+            ], [points]
+        );
+        const series = memoize(
+            () => ({
+                showPoints: false,
+            }),
+            []
+        );
 
-    const axes = React.useMemo(
-        () => [
-        { primary: true, type: 'utc', position: 'bottom' },
-        { type: 'linear', position: 'left' }
-        ],
-        []
-    );
+        const axes = memoize(
+            () => [
+            { primary: true, type: 'utc', position: 'bottom' },
+            { type: 'linear', position: 'left' }
+            ],
+            []
+        );
 
-    return (
-        // A react-chart hyper-responsively and continuously fills the available
-        // space of its parent element automatically
-        <div
-            style={{
-                width: '90%',
-                height: '300px',
-                paddingLeft: '50px',
-            }}
-        >
-            <ResizableArea
-                initHeight={{px: 0, percent: 100}}
-                initWidth={{px: 0, percent: 100}}
+        return (
+            // A react-chart hyper-responsively and continuously fills the available
+            // space of its parent element automatically
+            <div
+                style={{
+                    width: '90%',
+                    height: '300px',
+                }}
             >
-                <Chart data={data} series={series} axes={axes} tooltip primaryCursor secondaryCursor />
-            </ResizableArea>
-        </div>
-    );
+                <ResizableArea
+                    initHeight={{px: 0, percent: 100}}
+                    initWidth={{px: 0, percent: 100}}
+                >
+                    <Chart data={data()} series={series()} axes={axes()} tooltip primaryCursor secondaryCursor />
+                </ResizableArea>
+            </div>
+        );
+    }
 }
 
 export default PeopleInHospitalChart;
