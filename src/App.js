281f1b745b9974e2e7018006f29ad0c34aab2d64
Antoine Guenet <age@odoo.com>
2020-09-23 01:13:34 +0200

    [ADD] Charts: sliders

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import PeopleInHospitalChart from './components/PeopleInHospitalChart.js';
 import CasesByTestChart from './components/CasesByTestChart.js';
 import DataTable from './components/DataTable.js';
-import { LINK_HOSPI, LINK_TOTAL_TESTS, LINK_CASES } from './helpers';
+import InputRange from 'react-input-range';
+import { LINK_HOSPI, LINK_TOTAL_TESTS, LINK_CASES, getDateFrom } from './helpers';
+import 'react-input-range/lib/css/index.css';
 import './App.css';
 
 export default class App extends React.Component {
   state = {
     hospiWeeks: 3,
-    caseByTestsMonths: 3,
+    caseByTestsWeeks: 12,
   };
   render() {
     return (
@@ -25,14 +27,24 @@ export default class App extends React.Component {
 
         {this.state.data &&
         <section>
-          <h2>Patients at the hospital (last <input type="number" value={this.state.hospiWeeks} onChange={this._onChangeHospiWeeks.bind(this)}></input> weeks)</h2>
-          <PeopleInHospitalChart data={this.state.data} start={new Date().setDate(new Date().getDate() - (this.state.hospiWeeks * 7) - 1)} />
+          <h2>Patients at the hospital (last {this.state.hospiWeeks > 1 ? this.state.hospiWeeks + ' weeks' : 'week'})</h2>
+          <InputRange
+            minValue={1}
+            maxValue={this.state.weeksSinceStart}
+            value={this.state.hospiWeeks}
+            onChange={value => this.setState({ hospiWeeks: value })} />
+          <PeopleInHospitalChart data={this.state.data} start={getDateFrom(new Date(), -1 - (this.state.hospiWeeks * 7))} />
         </section>}
 
         {this.state.data &&
         <section>
-          <h2>Percentage of positive tests (last <input type="number" value={this.state.caseByTestsMonths} onChange={this._onChangeCaseByTestMonths.bind(this)}></input> months)</h2>
-          <CasesByTestChart data={this.state.data} start={new Date().setDate(new Date().getDate() - (this.state.caseByTestsMonths * 31) - 1)} />
+          <h2>Percentage of positive tests (last {this.state.caseByTestsWeeks > 1 ? this.state.caseByTestsWeeks + ' weeks' : 'week'})</h2>
+          <InputRange
+            minValue={1}
+            maxValue={this.state.weeksSinceStart}
+            value={this.state.caseByTestsWeeks}
+            onChange={value => this.setState({ caseByTestsWeeks: value })} />
+          <CasesByTestChart data={this.state.data} start={getDateFrom(new Date(), -1 - (this.state.caseByTestsWeeks * 7))} />
         </section>}
       </div>
     );
@@ -46,20 +58,19 @@ export default class App extends React.Component {
     const hospi = await (await fetch(LINK_HOSPI)).json();
     const tests = await (await fetch(LINK_TOTAL_TESTS)).json();
     const cases = await (await fetch(LINK_CASES)).json();
+    const firstDay = cases.reduce((a, b) => {
+      return Date.parse(a.DATE) > Date.parse(b.DATE) ? b : a;
+    }).DATE;
+    const weeksSinceStart = Math.ceil((new Date().getTime() - new Date(firstDay).getTime()) / (1000 * 60 * 60 * 24 * 7));
     this.setState({
       data: { hospi, tests, cases },
+      weeksSinceStart
     });
   }
-  _onChangeHospiWeeks(e) {
-    const value = +e.target.value;
-    this.setState({
-      hospiWeeks: value > 0 ? value : 0 || 0,
-    });
+  _onChangeHospiRange(value) {
+    this.setState({ hospiWeeks: value });
   }
-  _onChangeCaseByTestMonths(e) {
-    const value = +e.target.value;
-    this.setState({
-      caseByTestsMonths: value > 0 ? value : 0 || 0,
-    });
+  _onChangeCaseByTestRange(value) {
+    this.setState({ caseByTestsWeeks: value });
   }
 }
