dacd191741b7ceb3b2034ff7376f2c80e0905a1c
Antoine Guenet <age@odoo.com>
2020-10-01 13:16:45 +0200

    [ADD] News: RTBF Info

--- src/components/News.js
+++ src/components/News.js
@@ -6,11 +6,21 @@ const parser = new Parser();
 
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const sourcesUrls = {
-    vrt: 'https://www.vrt.be/vrtnws/en.rss.wetenschap.xml',
+    'vrt-science-en': 'https://www.vrt.be/vrtnws/en.rss.wetenschap.xml',
+    'vrt-wetenschap-nl': 'https://www.vrt.be/vrtnws/nl.rss.wetenschap.xml',
+    'rtbf-info': 'http://rss.rtbf.be/article/rss/rtbfinfo_homepage.xml'
 };
 export default class News extends React.Component {
+    state = {
+        unavailable: false,
+    }
     data;
     async componentDidMount() {
+        setTimeout(() => {
+            if (!this.data) {
+                this.setState({ unavailable: true });
+            }
+        }, 1000 * 30);
         this.data = await this._getAllData();
     }
     render() {
@@ -21,7 +31,7 @@ export default class News extends React.Component {
                     <ListItem key={item.id} alignItems="flex-start">
                         <ListItemText
                             primary={<Link href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</Link>}
-                            secondary={`${date} (${item.source})`}
+                            secondary={`${date} (${item.source.replace('-', ' ')})`}
                         />
                     </ListItem>
                 );
@@ -29,8 +39,11 @@ export default class News extends React.Component {
             return <List>
                 { listItems }
             </List>;
+        } else if (this.state.unavailable) {
+            return <p>Data unavailable. Please try again later.</p>;
+        } else {
+            return <Skeleton variant="rect" height={200} />;
         }
-        return <Skeleton variant="rect" height={200} />;
     }
     async _getData(url) {
         const feed = await parser.parseURL(proxy + url);
@@ -46,6 +59,6 @@ export default class News extends React.Component {
             }
             data = [...data, ...newData];
         }
-        return data.sort((a, b) => new Date(a.pubDate) - new Date(b.pubDate));
+        return data.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
     }
 }
