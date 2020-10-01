16dd7854d8d05da54a3050daedd66ea76cefdf89
Antoine Guenet <age@odoo.com>
2020-10-01 15:17:46 +0200

    fix news state

--- src/components/News.js
+++ src/components/News.js
@@ -11,24 +11,17 @@ const sourcesUrls = {
     'rtbf-info': 'http://rss.rtbf.be/article/rss/rtbfinfo_homepage.xml'
 };
 export default class News extends React.Component {
-    state = {
-        unavailable: false,
-    }
-    data;
+    state = {}
     async componentDidMount() {
-        setTimeout(() => {
-            if (!this.data) {
-                this.setState({ unavailable: true });
-            }
-        }, 1000 * 30);
-        this.data = await this._getAllData();
+        const data = await this._getAllData();
+        this.setState({ data });
     }
     render() {
-        if (this.data) {
-            const listItems = this.data.map(item => {
+        if (this.state.data) {
+            const listItems = this.state.data.map((item, index) => {
                 const date = new Date(item.pubDate).toDateString();
                 return (
-                    <ListItem key={item.id} alignItems="flex-start">
+                    <ListItem key={`${item.source}-${index}`} alignItems="flex-start">
                         <ListItemText
                             primary={<Link href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</Link>}
                             secondary={`${date} (${item.source.replace('-', ' ')})`}
@@ -39,15 +32,15 @@ export default class News extends React.Component {
             return <List>
                 { listItems }
             </List>;
-        } else if (this.state.unavailable) {
-            return <p>Data unavailable. Please try again later.</p>;
         } else {
             return <Skeleton variant="rect" height={200} />;
         }
     }
     async _getData(url) {
         const feed = await parser.parseURL(proxy + url);
-        const coronaItems = feed.items.filter(a => a.title.toLowerCase().includes('corona'));
+        const coronaItems = feed.items.filter(a => {
+            return a.title.toLowerCase().includes('corona') || a.description?.toLowerCase().includes('corona');
+        });
         return coronaItems;
     }
     async _getAllData() {
