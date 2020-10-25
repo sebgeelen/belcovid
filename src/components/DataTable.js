af12c5cff75f542888f28775a26c77b5d32d7ffc
Antoine Guenet <age@odoo.com>
2020-10-25 13:36:39 +0100

    [IMP] DataTable: replace tooltips with popovers

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -4,40 +4,61 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import { Link, Tooltip } from '@material-ui/core';
+import { Link } from '@material-ui/core';
+import { InfoBox } from './InfoBox';
 
-function saturationTooltip(icu = false) {
+function saturationPopover(icu = false) {
     let source;
     if (icu) {
         source = (<small><i>
-            Source on the number of beds in ICU: <Link href="https://www.vrt.be/vrtnws/en/2020/03/22/health-minister-says-that-an-additional-759-intensive-care-beds/">VRT, 2020.</Link>
+            Source on the number of beds in ICU:&nbsp;
+            <Link href={'https://www.vrt.be/vrtnws/en/2020/03/22/health-' +
+                'minister-says-that-an-additional-759-intensive-care-beds/'
+            }>
+                VRT,&nbsp;2020.
+            </Link>
         </i></small>);
     } else {
         source = (<small><i>
             Sources on the number of hospital beds and the average number of
-            hospitalized patients per day: <Link href="https://kce.fgov.be/sites/default/files/atoms/files/T%C3%A9l%C3%A9charger%20la%20synth%C3%A8se%20en%20fran%C3%A7ais%20%2884%20p.%29.pdf">KCE, 2014</Link> and
-            <Link href="https://www.healthybelgium.be/en/key-data-in-healthcare/general-hospitals/organisation-of-the-hospital-landscape/categorisation-of-hospital-activities/evolution-of-the-number-of-accredited-hospital-beds">healthybelgium.be</Link>.
+            hospitalized patients per day:&nbsp;
+            <Link href={'https://kce.fgov.be/sites/default/files/atoms/files/' +
+                'T%C3%A9l%C3%A9charger%20la%20synth%C3%A8se%20en%20' +
+                'fran%C3%A7ais%20%2884%20p.%29.pdf'}
+                target="_blank"
+            >
+                KCE,&nbsp;2014
+            </Link>
+                &nbsp;and&nbsp;
+            <Link href={'https://www.healthybelgium.be/en/key-data-in-' +
+                'healthcare/general-hospitals/organisation-of-the-hospital' +
+                '-landscape/categorisation-of-hospital-activities/evolution' +
+                '-of-the-number-of-accredited-hospital-beds'}
+                target="_blank"
+            >
+                healthybelgium.be
+            </Link>.
         </i></small>);
     }
     return (
-        <React.Fragment>
-            The day that all available ${icu ? 'ICU' : 'hospital'} beds would
-            be in use if the 7-day rolling average total number of COVID-19
-            patients in {icu ? 'ICU' : 'hospitals'} were to keep growing at
-            the same pace.<br/>
+        <InfoBox>
+            The day that all available {icu ? 'ICU' : 'hospital'} beds would be
+            in use if the 7-day rolling average total number of COVID-19
+            patients in {icu ? 'ICU' : 'hospitals'} were to keep growing at the
+            same pace.<br/>
             <br/>
             {source}
-        </React.Fragment>
+        </InfoBox>
     );
 }
-const CASES_TOOLTIP = 'Excluding the last 4 days, counting from today included, which are not yet consolidated.';
-function peakTooltip(peak) {
+function peakPopover(peak) {
     return (
-        <React.Fragment>
+        <InfoBox>
             The day that the number of people at the hospital would be the same
-            as on the highest day recorded ({peak.date.toDateString()}: {Math.round(peak.total)}),
-            based on 7-day rolling average to account for statistical noise.
-        </React.Fragment>
+            as on the highest day recorded
+            ({peak.date.toDateString()}:&nbsp;{Math.round(peak.total)}), based on
+            7-day rolling average to account for statistical noise.
+        </InfoBox>
     );
 }
 
@@ -58,10 +79,21 @@ export default class DataTable extends React.Component {
                         {
                             this.props.cases &&
                             <TableRow>
-                                <Tooltip title={CASES_TOOLTIP} placement="bottom-start" arrow>
-                                    <TableCell>Cases (last 7 days, daily average)</TableCell>
-                                </Tooltip>
-                                <TableCell>{Math.round(getAverageOver(this.props.cases, lastConsolidatedDataDay(), -7))}</TableCell>
+                                <TableCell>
+                                    Cases (last 7 days, daily average)
+                                    <InfoBox>
+                                        Excluding the last 4 days, counting from
+                                        today included, which are not yet
+                                        consolidated.
+                                    </InfoBox>
+                                </TableCell>
+                                <TableCell>
+                                    {Math.round(getAverageOver(
+                                        this.props.cases,
+                                        lastConsolidatedDataDay(),
+                                        -7,
+                                    ))}
+                                </TableCell>
                             </TableRow>
                         }
                         {
@@ -76,9 +108,10 @@ export default class DataTable extends React.Component {
                         {
                             this.props.totalHospitalizations &&
                             <TableRow>
-                                <Tooltip title={peakTooltip(this.peakHospitalizations)} placement="bottom-start" arrow>
-                                    <TableCell>Day of new total in hospital peak (at current rate)</TableCell>
-                                </Tooltip>
+                                <TableCell>
+                                    Day of new total in hospital peak
+                                    (at current rate)&nbsp;{peakPopover(this.peakHospitalizations)}
+                                </TableCell>
                                 <TableCell>{
                                     this.getSaturationDay(
                                         this.props.totalHospitalizations,
@@ -90,9 +123,10 @@ export default class DataTable extends React.Component {
                         {
                             this.props.totalHospitalizations &&
                             <TableRow>
-                                <Tooltip title={saturationTooltip()} placement="bottom-start" arrow>
-                                    <TableCell>Day of hospital saturation (at current rate)</TableCell>
-                                </Tooltip>
+                                <TableCell>
+                                    Day of hospital saturation
+                                    (at current rate)&nbsp;{saturationPopover()}
+                                </TableCell>
                                 <TableCell>{
                                     this.getSaturationDay(
                                         this.props.totalHospitalizations,
@@ -104,9 +138,10 @@ export default class DataTable extends React.Component {
                         {
                             this.props.totalICU &&
                             <TableRow>
-                                <Tooltip title={saturationTooltip(true)} placement="bottom-start" arrow>
-                                    <TableCell>Day of ICU saturation (at current rate)</TableCell>
-                                </Tooltip>
+                                <TableCell>
+                                    Day of ICU saturation
+                                    (at current rate)&nbsp;{saturationPopover(true)}
+                                </TableCell>
                                 <TableCell>{
                                     this.getSaturationDay(
                                         this.props.totalICU,
---
+++ src/components/InfoBox.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+import { Box, IconButton, Popover, Typography } from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
+
+export class InfoBox extends React.Component {
+    render() {
+        return (
+            <PopupState variant="popover" popupId="demo-popup-popover">
+                {(popupState) => (
+                    <React.Fragment>
+                        <IconButton
+                            color="primary"
+                            style={{verticalAlign: 'super', padding: 0}}
+                            {...bindTrigger(popupState)}
+                        >
+                            <InfoIcon style={{fontSize: 15 }} />
+                        </IconButton>
+                        <Popover
+                            {...bindPopover(popupState)}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                                }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                        >
+                            <Box p={2}>
+                            <Typography style={{
+                                width: 400,
+                                maxWidth: '90%',
+                                fontSize: 'small'}}
+                            >
+                                {this.props.children}
+                            </Typography>
+                            </Box>
+                        </Popover>
+                    </React.Fragment>
+                )}
+            </PopupState>
+        );
+    }
+}
